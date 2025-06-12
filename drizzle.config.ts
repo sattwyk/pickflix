5d84e9d163ea34313491b7113f4fc43ccec82d48
Sattwik Biswal <sattwyk@gmail.com>
2025-06-12 15:58:59 +0530

    setup drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { env } from '@/env';
+import { defineConfig } from 'drizzle-kit';
+export default defineConfig({
+  out: './db/migrations',
+  schema: './db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
