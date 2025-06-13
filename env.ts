f6f080dffe9d8abaec8cca778fded4a817481b56
Sattwik Biswal <sattwyk@gmail.com>
2025-06-13 07:59:34 +0530

    add tmdb envs

--- env.ts
+++ env.ts
@@ -5,12 +5,15 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
     CLERK_SECRET_KEY: z.string().min(1),
+    TMDB_READ_ACESS_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_TMDB_API_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
   },
 });
